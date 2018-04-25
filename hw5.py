4018e0df283b8924a26e8082a2e1fd3c0b138179
106070020 <36986296+106070020@users.noreply.github.com>
2018-04-25 15:28:14 +0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -12,12 +12,13 @@ def main(filename):
     for line in lines:
         # split a line of text into a list words
         # "I have a dream." => ["I", "have", "a", "dream."]
+        line= line.strip()
         words = line.split()
 
         # check the format of words and append it to "all_words" list
         for word in words:
             word = word.strip(string.punctuation)
-            if word != " " :
+            if word != "" :
                 all_words.append(word)
                 
     # compute word count from all_words
